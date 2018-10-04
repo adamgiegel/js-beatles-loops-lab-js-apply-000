88c0d7daebb5855c68143d4ebf9139f400be4b9c
Adam Giegel <adam.giegel@gmail.com>
2018-10-04 14:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,27 @@
 function theBeatlesPlay (musicians, instruments){
   let newArr = []
   for(let i = 0; i < musicians.length, i < instruments.length; i++){
-    newArr.push(musicians[i] + "plays" + instruments[i])
+    newArr.push(musicians[i] + " plays " + instruments[i])
   }
+  return newArr
+}
+
+function johnLennonFacts (array){
+  let newArr = []
+  let i = 0
+  while(i < array.length ){
+    newArr.push(array[i] + "!!!")
+    i++
+  }
+  return newArr
+}
+
+function iLoveTheBeatles (num){
+  let newArr = []
+  let i = num
+  do{
+    i = i + 1
+    newArr.push("I love the Beatles!")
+  } while (i < 15);
+  return newArr
 }
